f4dbfe9ffd3a88d251c964df0ad45160f78e9d60
Anatoly Ivanov <anatoly.ivanov@gmail.com>
2022-04-06 17:09:24 -0700

    fixing a bug

--- s1/c5_rep_key_xor_test.go
+++ s1/c5_rep_key_xor_test.go
@@ -31,16 +31,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func EncryptRepeatedKeyXor(plaintext, key string) string {
+func EncryptRepeatedKeyXor(plaintext, key string) []byte {
 	resultBytes := make([]byte, len(plaintext))
 	keyCounter := 0
 
 	for i := range plaintext {
-		resultBytes[i] = plaintext[i] ^ key[keyCounter%3]
+		resultBytes[i] = plaintext[i] ^ key[keyCounter%len(key)]
 		keyCounter++
 	}
-	return hex.EncodeToString(resultBytes)
-
+	return resultBytes
 }
 
 func TestEncryptRepeatedKeyXor(t *testing.T) {
@@ -48,7 +47,7 @@ func TestEncryptRepeatedKeyXor(t *testing.T) {
 	const plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 	const hexResult = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
-	ciphertext := EncryptRepeatedKeyXor(plaintext, "ICE")
+	ciphertext := hex.EncodeToString(EncryptRepeatedKeyXor(plaintext, "ICE"))
 	assert.Equal(t, hexResult, ciphertext)
 
 }
